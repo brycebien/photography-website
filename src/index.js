eb2dc6de15e117123698c273a18f5dac265bd11d
brycebien <brycebien@gmail.com>
2026-03-25 19:28:04 -0400

    initial page setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('hello world');
+const { readFile, readFileSync } = require('fs');
+const express = require('express');
+const app = express();
+
+app.get('/', (request, response) => {
+	readFile('./src/views/index.html', 'utf8', (err, html) => {
+		if (err) {
+			response.status(500).send(`An error occured: ${err}`);
+		}
+
+		response.send(html);
+	});
+});
+
+app.listen(process.env.PORT || 3000, () => console.log(`App available on port ${process.env.PORT || 3000}`));
