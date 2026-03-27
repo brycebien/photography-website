548d6dc4cda01a70ba80c75e3cfb446d6864effb
brycebien <brycebien@gmail.com>
2026-03-27 13:01:32 -0400

    remove dead code

--- src/index.js
+++ src/index.js
@@ -23,31 +23,4 @@ app.use(express.static(__dirname));
 app.use('/', indexRouter);
 app.use('/contact', indexRouter);
 
-// app.get('/', (request, response) => {
-// 	readFile('./src/views/index.html', 'utf8', (err, html) => {
-// 		if (err) {
-// 			response.status(500).send(`An error occured: ${err}`);
-// 		}
-
-// 		response.send(html);
-// 	});
-// });
-
-// app.get('/contact', (request, response) => {
-// 	readFile('./src/views/contact.html', 'utf8', (err, html) => {
-// 		if (err) {
-// 			response.status(500).send(`An error occured: ${err}`);
-// 		}
-
-// 		response.send(html);
-// 	});
-// });
-
-// app.post('/contact', (request, response) => {
-// 	const { firstName, lastName, email, message } = request.body;
-
-// 	console.log(firstName);
-// 	response.redirect('/');
-// });
-
 app.listen(process.env.PORT || 3000, () => console.log(`App available on port ${process.env.PORT || 3000}`));
